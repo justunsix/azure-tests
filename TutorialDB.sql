2f8cd5514cc1ae273defa9cc0565a7ff2e3f1a2d
Justin Tung <justin.tung@live.ca>
2021-07-17 14:34:45 -0400

    tutorial database queries and setup

---
+++ TutorialDB.sql
@@ -0,0 +1,45 @@
+-- Create a new database called 'TutorialDB'
+-- from https://docs.microsoft.com/en-us/sql/tools/visual-studio-code/sql-server-develop-use-vscode?view=sql-server-ver15
+-- Connect to the 'master' database to run this snippet
+USE master
+GO
+-- Create the new database if it does not exist already
+IF NOT EXISTS (
+    SELECT name
+FROM sys.databases
+WHERE name = N'TutorialDB'
+)
+CREATE DATABASE TutorialDB
+GO
+
+-- Create a new table called 'Employees' in schema 'dbo'
+-- Drop the table if it already exists
+IF OBJECT_ID('dbo.Employees', 'U') IS NOT NULL
+DROP TABLE dbo.Employees
+GO
+-- Create the table in the specified schema
+CREATE TABLE dbo.Employees
+(
+    EmployeesId INT NOT NULL PRIMARY KEY,
+    Name [NVARCHAR](50) NOT NULL,
+    Location [NVARCHAR](50) NOT NULL
+    -- specify more columns here
+);
+GO
+
+-- Insert rows into table 'Employees'
+INSERT INTO Employees
+   ([EmployeesId],[Name],[Location])
+VALUES
+   ( 1, N'Jared', N'Australia'),
+   ( 2, N'Nikita', N'India'),
+   ( 3, N'Tom', N'Germany'),
+   ( 4, N'Jake', N'United States')
+GO
+-- Query the total count of employees
+SELECT COUNT(*) as EmployeeCount FROM dbo.Employees;
+-- Query all employee information
+SELECT e.EmployeesId, e.Name, e.Location
+FROM dbo.Employees as e
+GO
+
